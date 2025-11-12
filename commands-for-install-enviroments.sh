f7b225fa567de4f0b2b4586168d9c6f60e8dcc7e
Anita-Anahit <Anahit.Mkrtchyan@optimumpartners.com>
2025-11-12 12:16:25 +0400

    All necessary files after running dependency and library using commands for task implementation.

---
+++ commands-for-install-enviroments.sh
@@ -0,0 +1,17 @@
+npm create vite@latest .
+
+npm install
+
+npm install @mui/material @mui/icons-material @emotion/react @emotion/styled
+
+npm install react-router-dom
+
+npm install zustand
+
+npm install react-hook-form @hookform/resolvers yup
+
+npm install uuid
+
+npm install --save-dev @types/uuid
+
+npm install @mui/lab
